aa9975d8bd0a3c33009cfc1ad09d4b1d2e7459b1
Анна Сергеева <ancitrus@gmail.com>
2026-04-05 21:20:28 +0300

    validations

--- obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f279148755c07f493c7702f72b3116b35c0cdaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a864a23f40eab082a89aae6a62d3e5c25306a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
