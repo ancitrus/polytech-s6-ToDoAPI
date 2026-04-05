c2fece3e772d7f9261b08f26c59629db8ae0bf83
Анна Сергеева <ancitrus@gmail.com>
2026-04-05 16:10:30 +0300

    validations

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b46401caa8495da9072f3b68be37fbf53a5179b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f29c7674d40ab86e7af6b2c96b1c22df0c4939")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
