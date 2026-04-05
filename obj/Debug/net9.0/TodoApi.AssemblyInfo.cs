9b439680f2ba617787aab3450cea9a79d9e11ef0
Анна Сергеева <ancitrus@gmail.com>
2026-04-05 12:10:10 +0300

    DTO

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24104841e8624da8b949d4e1756cf82e4c2e1492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd38475807e5c462d6fe7d21b9125fb22a610a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
